a7a034a2118d72fa648fc1e3421b51b7ab6645e7
AndreaMargutti <marguttiandrea30@gmail.com>
2024-10-10 15:38:07 +0200

    created product obj

--- classes/product.php
+++ classes/product.php
@@ -1,1 +1,14 @@
 <?php 
+
+class Product {
+    public string $name;
+    public float $price;
+    public string $locate;
+
+    public function __construct($name, $price, $locate)
+    {
+      $this->name = $name;
+      $this->price = $price;
+      $this->locate = $locate;
+    }
+}
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-
+require_once __DIR__ . "../classes/Product.php";
 ?>
 
 <!DOCTYPE html>
@@ -8,11 +8,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Bootstrap-->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css'
+        integrity='sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg=='
+        crossorigin='anonymous' />
     <title>php-oop-2</title>
 </head>
 
 <body>
-
+    <main class="container text-center py-2">
+        <h1>E-Commerce OOP</h1>
+        <div class="card mt-4" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title"></h5>
+                <p class="card-text">
+                    <?php
+                    var_dump(new Product("Cibo", 43, "scaffale 1b"))
+                    ?>
+                </p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item"></li>
+                <li class="list-group-item"></li>
+                <li class="list-group-item"></li>
+            </ul>
+        </div>
+    </main>
 </body>
 
 </html>
