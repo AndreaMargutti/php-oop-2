b7f70919a6925fc62b36860a28d4c05a1718d7a7
Andrea Margutti <marguttiandrea30@gmail.com>
2024-10-11 17:29:32 +0200

    started customize layout

--- index.php
+++ index.php
@@ -16,80 +16,81 @@ require_once __DIR__ . "/db/data.php";
 </head>
 
 <body>
-    <main class="container text-center py-2">
+    <main class="container text-center my-4">
         <h1>E-Commerce OOP</h1>
-        <div class="row"></div>
-        <?php foreach($products as $product) { ?>
-        <div class="col">
-            <div class="card mt-4">
-                <div class="card-body">
-                    <h5 class="card-title">
-                        <?php echo $product->name; ?>
-                    </h5>
+        <section class="row row-cols-2">
+            <?php foreach($products as $product) { ?>
+            <div class="col my-3">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <?php echo $product->name; ?>
+                        </h5>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">
+                            Prezzo:
+                            <?php echo $product->price; ?>
+                        </li>
+                        <li class="list-group-item">
+                            Quantità Disponibile:
+                            <?php echo $product->inStock; ?>
+                        </li>
+                        <li class="list-group-item">
+                            <?php echo $product->locate; ?>
+                        </li>
+                        <li class="list-group-item">
+                            <?php echo $product->category->type; ?>
+                            <br>
+                            <img src="<?php echo $product->category->icon; ?>" alt="dog icon" width="5%">
+                        </li>
+                        <!--Inizio specifiche per cibo-->
+                        <?php if(is_a($product, "Food")) { ?>
+                        <li class="list-group-item">
+                            <?php echo $product->brand; ?>
+                        </li>
+                        <li class="list-group-item">
+                            <?php echo $product->solidity; ?>
+                        </li>
+                        <li class="list-group-item">
+                            <?php echo $product->size; ?>
+                        </li>
+                        <!--Nel caso in cui fosse un gioco...-->
+                        <?php } else if (is_a($product, "Game")){ ?>
+                        <li class="list-group-item">
+                            <?php if($product->sound) {
+                            echo "Rumoroso";
+                            } else {
+                                echo "Silenzioso";
+                            } ?>
+                        </li>
+                        <li class="list-group-item">
+                            <?php echo $product->gameType; ?>
+                        </li>
+                        <?php } else if(is_a($product, "Kennel")) {  ?>
+                        <li class="list-group-item">
+                            <?php echo $product->height ?>
+                        </li>
+                        <li class="list-group-item">
+                        <?php echo $product->width ?>
+                        </li>
+                        <?php } ?>
+                        <li class="list-group-item text-primary">
+                            <?php echo $product->possibleSellers; ?>
+                        </li>
+                        <li class="list-group-item text-primary">
+                            <!--Esegui la funzione e, se non riesci, lancia l'eccezzione!-->
+                            <?php try {
+                                echo $product->buy(-4);       
+                            } catch (ArithmeticError $e) {
+                                echo $e->getMessage();
+                            } ?>
+                        </li>
+                    </ul>
                 </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">
-                        Prezzo:
-                        <?php echo $product->price; ?>
-                    </li>
-                    <li class="list-group-item">
-                        Quantità Disponibile:
-                        <?php echo $product->inStock; ?>
-                    </li>
-                    <li class="list-group-item">
-                        <?php echo $product->locate; ?>
-                    </li>
-                    <li class="list-group-item">
-                        <?php echo $product->category->type; ?>
-                        <br>
-                        <img src="<?php echo $product->category->icon; ?>" alt="dog icon" width="5%">
-                    </li>
-                    <!--Inizio specifiche per cibo-->
-                    <?php if(is_a($product, "Food")) { ?>
-                    <li class="list-group-item">
-                        <?php echo $product->brand; ?>
-                    </li>
-                    <li class="list-group-item">
-                        <?php echo $product->solidity; ?>
-                    </li>
-                    <li class="list-group-item">
-                        <?php echo $product->size; ?>
-                    </li>
-                    <!--Nel caso in cui fosse un gioco...-->
-                    <?php } else if (is_a($product, "Game")){ ?>
-                    <li class="list-group-item">
-                        <?php if($product->sound) {
-                        echo "Rumoroso";
-                        } else {
-                            echo "Silenzioso";
-                        } ?>
-                    </li>
-                    <li class="list-group-item">
-                        <?php echo $product->gameType; ?>
-                    </li>
-                    <?php } else if(is_a($product, "Kennel")) {  ?>
-                    <li class="list-group-item">
-                        <?php echo $product->height ?>
-                    </li>
-                    <li class="list-group-item">
-                    <?php echo $product->width ?>
-                    </li>
-                    <?php } ?>
-                    <li class="list-group-item text-primary">
-                        <?php echo $product->possibleSellers; ?>
-                    </li>
-                    <li class="list-group-item text-primary">
-                        <!--Esegui la funzione e, se non riesci, lancia l'eccezzione!-->
-                        <?php try {
-                            echo $product->buy(-4);       
-                        } catch (ArithmeticError $e) {
-                            echo $e->getMessage();
-                        } ?>
-                    </li>
-                </ul>
             </div>
-        </div>
-        <?php } ?>
+            <?php } ?>
+        </section>
     </main>
 </body>
 
