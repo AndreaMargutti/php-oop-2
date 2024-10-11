b726adf18bacb9e289e4a3483f60a75ca0825da8
Andrea Margutti <marguttiandrea30@gmail.com>
2024-10-11 16:35:37 +0200

    added instance variables in Kennel

--- classes/Kennel.php
+++ classes/Kennel.php
@@ -1,5 +1,13 @@
 <?php
 
 class Kennel extends Product {
-    
+    public int $height;
+    public int $width;
+
+    public function __construct($name, $price, $inStock, $locate, $category, $height, $width)
+    {
+     parent::__construct($name, $price, $inStock, $locate, $category);
+     $this->height = $height;
+     $this->width = $width;
+    }
 }
